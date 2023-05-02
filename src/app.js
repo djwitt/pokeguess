c804e562586ed114c885cbd43f299b12cb84c81d
dwitt.ae <dwitt.ae@gmail.com>
2023-05-01 22:03:43 -0500

    Update EventListeners and Formatting
    - Add new eventListener for transitions.
    - Update formatting to add in semicolons.
    - Remove some JSDocs comments

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const pokemon = document.querySelector(".pokemon")
-const showBtn = document.querySelector(".show")
-const nextBtn = document.querySelector(".next-pokemon")
-const pokeName = document.querySelector(".pokemon-name")
-const pokeAPI = "https://pokeapi.co/api/v2/pokemon/"
-const maxPokemon = 1010
-let fullPokemonName
+const pokemon = document.querySelector(".pokemon");
+const showBtn = document.querySelector(".show");
+const nextBtn = document.querySelector(".next-pokemon");
+const pokeName = document.querySelector(".pokemon-name");
+const pokeAPI = "https://pokeapi.co/api/v2/pokemon/";
+const maxPokemon = 1010;
+let fullPokemonName;
 
 /**
  *
@@ -14,88 +14,74 @@ let fullPokemonName
  */
 function adjustFilter(action, text = undefined) {
     if (action === "show" && text === "text") {
-        return "opacity(1)"
+        return "opacity(1)";
     }
 
     if (action === "next" && text === "text") {
-        return "opacity(0)"
+        return "opacity(0)";
     }
 
     if (action === "show") {
-        return "opacity(1) brightness(1)"
+        return "opacity(1) brightness(1)";
     }
 
     if (action === "hide") {
-        return "opacity(1) brightness(0)"
+        return "opacity(1) brightness(0)";
     }
 
     if (action === "next") {
-        return "opacity(0) brightness(0)"
+        return "opacity(0) brightness(0)";
     }
 }
 
 function randomPokemonID(size) {
-    return Math.floor(Math.random() * size)
+    return Math.floor(Math.random() * size);
 }
 
-/**
- *
- * @param {string} imageURL Requires image URL from PokeAPI
- * @returns {void}
- */
 function renderPokemon(imageURL) {
-    pokemon.style.background = `url(${imageURL}) center/contain no-repeat`
-    if (pokemon.style.background) {
-        // Uneducated hack to smooth out transition between when the URL is called to when it is painted.
-        setTimeout(() => {
-            pokemon.style.filter = adjustFilter("hide")
-        }, 200)
-    }
+    pokemon.style.filter = adjustFilter("hide");
+    pokemon.style.background = `url(${imageURL}) center/contain no-repeat`;
 }
 
-/**
- *
- * @param {string} URL API URL
- * @returns {Promise}
- */
 async function fetchPokemon(URL) {
     try {
-        const request = await fetch(URL)
-        const json = await request.json()
-        return json
+        const request = await fetch(URL);
+        const json = await request.json();
+        return json;
     } catch (err) {
-        throw new Error(err)
+        throw new Error(err);
     }
 }
 
-/**
- *
- * @param {number} pokeID Accepts a number for grabbing a random pokemon index
- * @returns {void}
- */
 async function getPokemon(pokeID) {
-    const pokemonResponse = await fetchPokemon(`${pokeAPI}${pokeID}`)
+    const pokemonResponse = await fetchPokemon(`${pokeAPI}${pokeID}`);
     const pokemonURL = await pokemonResponse?.sprites.other["official-artwork"]
-        .front_default
-    fullPokemonName = pokemonResponse.name
-    renderPokemon(pokemonURL)
+        .front_default;
+    fullPokemonName = pokemonResponse.name;
+    renderPokemon(pokemonURL);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    pokemon.style.setProperty("filter", adjustFilter("next"))
-    getPokemon(randomPokemonID(maxPokemon))
-})
+    pokemon.style.filter = adjustFilter("next");
+    getPokemon(randomPokemonID(maxPokemon));
+});
 
 showBtn.addEventListener("click", () => {
-    pokemon.style.filter = adjustFilter("show")
-    pokeName.innerHTML = fullPokemonName
-    pokeName.style.filter = adjustFilter("show", "text")
-})
+    pokemon.style.filter = adjustFilter("show");
+    pokeName.innerHTML = fullPokemonName;
+    pokeName.style.filter = adjustFilter("show", "text");
+});
 
 nextBtn.addEventListener("click", () => {
-    pokemon.style.filter = adjustFilter("next")
-    pokeName.style.filter = adjustFilter("next", "text")
-    setTimeout(() => {
-        getPokemon(randomPokemonID(maxPokemon))
-    }, 800)
-})
+    pokeName.style.filter = adjustFilter("next", "text");
+    pokemon.style.filter = adjustFilter("next");
+
+    pokemon.addEventListener(
+        "transitionend",
+        (e) => {
+            console.log(e);
+            getPokemon(randomPokemonID(maxPokemon));
+        },
+        { once: true }
+    );
+});
